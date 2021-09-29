f7b1edcb756c09fd2bbc4d5f84cfb30d783b410c
German Volkov <volkov97@yandex-team.ru>
2021-09-29 15:43:25 +0300

    example folder

--- example/index.js
+++ example/index.js
@@ -1,20 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-const { replaceBackground } = require('../build');
+const { replaceBackground } = require('backrem');
 
 const catExample = fs.createReadStream(
-  path.resolve(__dirname, "../examples/cat.jpg")
+  path.resolve(__dirname, "./target/cat.jpg")
 );
 
 const spaceExample = fs.createReadStream(
-  path.resolve(__dirname, "../examples/space.jpg")
+  path.resolve(__dirname, "./target/space.jpg")
 );
 
 replaceBackground(catExample, spaceExample, [200, 50, 50], 3).then(
   (readableStream) => {
     const writableStream = fs.createWriteStream(
-      path.resolve(__dirname, "../dist/result.jpg")
+      path.resolve(__dirname, "./result/result.jpg")
     );
 
     readableStream.pipe(writableStream);
